10fbecc8d70b3a7e3d48c0ceae40faa799dad863
Max Cardenas <max.cardenas.cas@gmail.com>
2024-12-02 17:13:16 -0500

    chore: connect login to backend

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,2 +1,2 @@
-export const API_BASE_URL = 'https://baseurl.co';
+export const API_BASE_URL = 'https://autfmibackendstaging-axf5cac2b3c0g0f0.brazilsouth-01.azurewebsites.net';
 export const TOKEN = 'jwt_token';

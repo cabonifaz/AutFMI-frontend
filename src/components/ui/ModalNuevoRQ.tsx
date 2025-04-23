4b378d4a6aa54f8d603e704deb3a55414e2c6623
max cardenas <max.cardenas.cas@gmail.com>
2025-04-23 16:06:15 -0500

    refactor (OBS-FTBO-0009): set 'Autogenerado' to codigoRQ when auto generate is checked

--- src/components/ui/ModalNuevoRQ.tsx
+++ src/components/ui/ModalNuevoRQ.tsx
@@ -289,7 +289,7 @@ export const AgregarRQModal = ({ onClose, updateRQData, estadoOptions, clientes,
                                                     type="checkbox"
                                                     onChange={(e) => {
                                                         setAutogenRQ(e.target.checked);
-                                                        setValue("codigoRQ", e.target.checked ? "" : getValues("codigoRQ"));
+                                                        setValue("codigoRQ", e.target.checked ? "Autogenerado" : "");
                                                         clearErrors("codigoRQ");
                                                     }}
                                                     className="input-checkbox"

c760fada0acbd3d1829e9b6fd47e935ffa8e7ac2
Max Cardenas <max.cardenas.cas@gmail.com>
2025-04-28 17:46:25 -0500

    tickets OBS-FTBO-0021 and OBS-FTBO-0022 add new column Asignado and add a checkbox in contact modal

--- src/models/response/ClientContactsResponse.ts
+++ src/models/response/ClientContactsResponse.ts
@@ -1,7 +1,7 @@
-import { ClientContact } from "../type/ClientContact";
+import { ReqContacto } from "../type/ReqContacto";
 
 export interface ClientContactResponse {
     idTipoMensaje: number;
     mensaje: string;
-    lstClientContacts: ClientContact[]
+    lstClientContacts: ReqContacto[]
 }

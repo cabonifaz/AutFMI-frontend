83d053aeb4aa77c85a782b891b47b52d3191c3f5
Denis I. A. <dial1901110514@gmail.com>
2025-07-11 13:10:17 -0500

    Update URL AWS UAT backend

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL = 'https://autfmibackendstaging-axf5cac2b3c0g0f0.brazilsouth-01.azurewebsites.net';
+export const API_BASE_URL = 'https://api-fmi.aplicacion2-team.com';
 export const TOKEN = 'jwt_token';
 
 // PARAMS

a870af66a5b99fa76ae3678115ba1d951d34f5d7
max cardenas <max.cardenas.cas@gmail.com>
2025-07-15 17:00:27 -0500

    fix: add form to ensure files update in RQ

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,5 +1,5 @@
-export const API_BASE_URL = 'https://api-fmi.aplicacion2-team.com';
-export const TOKEN = 'jwt_token';
+export const API_BASE_URL = "https://api-fmi.aplicacion2-team.com";
+export const TOKEN = "jwt_token";
 
 // PARAMS
 export const TIPO_TIEMPO = "5";
@@ -9,17 +9,16 @@ export const TIPO_MODAL_MODALIDAD = "6";
 export const UNIDAD = "7";
 export const MOTIVO_INGRESO = "8";
 export const MOTIVO_CESE = "10";
-export const TIPO_EQUIPO = '2';
-export const MARCA_EQUIPO = '5';
-export const TIPO_HARDWARE = '21';
-export const ANEXO_HARDWARE = '22';
-export const TIPO_SOFTWARE = '23';
-export const ESTADO_RQ = '24';
-export const PERFIL = '14';
-export const DURACION_RQ = '28';
-export const LIMITE_ALERTA_RQ = '30';
-export const MODALIDAD_RQ = '31';
-
+export const TIPO_EQUIPO = "2";
+export const MARCA_EQUIPO = "5";
+export const TIPO_HARDWARE = "21";
+export const ANEXO_HARDWARE = "22";
+export const TIPO_SOFTWARE = "23";
+export const ESTADO_RQ = "24";
+export const PERFIL = "14";
+export const DURACION_RQ = "28";
+export const LIMITE_ALERTA_RQ = "30";
+export const MODALIDAD_RQ = "31";
 
 // RQ TALENT STATE
 export const ESTADO_OBSERVADO = 1;

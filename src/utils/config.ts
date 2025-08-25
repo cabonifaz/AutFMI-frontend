a0a7308d27f3878f6458743373ee45691075d81b
Denis I. A. <dial1901110514@gmail.com>
2025-08-25 09:43:39 -0500

    Config Prod AWS api

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,5 +1,4 @@
-export const API_BASE_URL = "https://api-autfmi.aplicacion2-team.com";
-// export const API_BASE_URL = "http://localhost:8081";
+export const API_BASE_URL = "https://api-autfmi.fractal.com.pe";
 export const TOKEN = "jwt_token";
 
 // PARAMS

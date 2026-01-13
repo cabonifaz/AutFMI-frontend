a08da3cc55fd42b414eda240c63a43f659e5e382
zero <himura.js@hotmail.com>
2026-01-12 19:25:49 -0500

    feat: Employee Name to modal

--- src/components/ui/modals/EmpleoyeeDetails/MDEmployeeDetails.tsx
+++ src/components/ui/modals/EmpleoyeeDetails/MDEmployeeDetails.tsx
@@ -25,7 +25,9 @@ export const MDEmployeeDetails = ({ onClose, talento }: MDProps) => {
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-40">
       <div className="bg-white rounded-lg shadow-lg p-4 w-full md:w-[95%] lg:w-[1300px] min-h-[570px] max-h-[570px] overflow-hidden relative">
         <header className="flex items-center justify-between mb-2">
-          <h2 className="text-lg font-bold">Detalles Empleado</h2>
+          <h2 className="text-lg font-bold">
+            Detalles Empleado - {details?.names || "-"}
+          </h2>
           <CloseModalButton onClick={onClose} />
         </header>
         <Tabs

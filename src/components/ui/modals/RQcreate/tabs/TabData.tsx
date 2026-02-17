0cdd39fe06764ea47b1a76487820d564b76b9780
zero <himura.js@hotmail.com>
2026-02-17 11:45:38 -0500

    feat:Centralize the submit for all tabs

--- src/components/ui/modals/RQcreate/tabs/TabData.tsx
+++ src/components/ui/modals/RQcreate/tabs/TabData.tsx
@@ -92,10 +92,7 @@ export const TabData = ({ rqStates }: TabProps) => {
               type="checkbox"
               onChange={(e) => {
                 setAutogenRQ(e.target.checked);
-                setValue(
-                  "codigoRQ",
-                  e.target.checked ? "Autogenerado" : ""
-                );
+                setValue("codigoRQ", e.target.checked ? "Autogenerado" : "");
                 clearErrors("codigoRQ");
               }}
               className="input-checkbox"
@@ -182,13 +179,6 @@ export const TabData = ({ rqStates }: TabProps) => {
             </p>
           )}
         </div>
-      </div>
-
-      {/* Botones de acción */}
-      <div className="flex justify-end space-x-4 mt-6 me-1">
-        <button type="submit" className="btn btn-primary">
-          Agregar RQ
-        </button>
       </div>
     </div>
   );
